204fb8ec6faba4c1c2fb0d08ae6294ae15e018ce
yeahzxnn <yejin0621@tukorea.ac.kr>
2023-01-11 22:47:58 +0900

    UI 완료 후 코드 구현중입니다.

---
+++ Fish-ShapedBunMakingGame/GameView/GameViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  Fish-ShapedBunMakingGame
+//
+//  Created by 신예진 on 2023/01/09.
+//
+
+import UIKit
+import AVFoundation
+
+class GameViewController: UIViewController{
+    
+    //UI연결
+    
+    //붕어빵 개수
+    @IBOutlet weak var customerOrder: UILabel!
+    
+    //목숨 개수
+    @IBOutlet weak var heart1: UIImageView!
+    @IBOutlet weak var heart2: UIImageView!
+    @IBOutlet weak var heart3: UIImageView!
+    
+    //scorelabel
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    //재료
+    @IBOutlet weak var FishDoughButton: UIButton!
+    @IBOutlet weak var redBeanButton: UIButton!
+    @IBOutlet weak var handButton: UIButton!
+    
+    //붕어빵 틀
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    
+    
+    
+    //붕어빵판 코드
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- Fish-ShapedBunMakingGame/StartView/ViewController.swift
+++ Fish-ShapedBunMakingGame/StartView/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+     //화면 시작하자마자 하는 방법 바로 나오고 나는 Start누르고 시작하면 됨!ㅊ
+    @IBAction func clickedGameStart(_ sender: Any) {
+      let vc = GameViewController()
+      present(vc, animated: false, completion: nil)
+    }
+    
 }
 
